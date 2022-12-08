db60019a58bd3b5f00f83c1cab8af531523984c9
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2022-12-09 00:57:23 +0900

    Remove unnecessary code

--- launch_labelmaker.sh
+++ launch_labelmaker.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 DOCKER_IMAGE="labelmaker:latest"
 
-id=${1:-"1"}
-
-gpu="--gpus=all"
-if ! command -v nvidia-smi &> /dev/null
-then
-    echo "NVIDIA Driver not be found"
-    gpu=""
-fi
-
 xhost +
-docker run $gpu -it --rm --privileged \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+docker run -it --rm --privileged \
     -u `id -u`:`id -g` \
+    -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     --name labelmaker \
     --device /dev/dri \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v $PWD/label_image:/YLLabelMaker/label_image:rw \
     $DOCKER_IMAGE 
